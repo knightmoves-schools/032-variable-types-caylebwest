868723378463791737631ee311e1e2f612f976ea
Cayleb West <cwest@anamosa.k12.ia.us>
2026-04-14 11:31:15 -0500

    Add animalName to result display in index.js

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ let isIndoorPet;
 document.getElementById("string").innerHTML = typeOf animalName;
 document.getElementById("number").innerHTML = typeOf height;
 document.getElementById("boolean").innerHTML = typeOf isIndoorPet;
+document.getElementById('result').innerHTML = animalName;
