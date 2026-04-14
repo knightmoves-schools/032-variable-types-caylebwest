af62dbaeddddc72aeadaceaef52be4e7be59f14e
Cayleb West <cwest@anamosa.k12.ia.us>
2026-04-14 11:27:49 -0500

    Initialize animal variables and display their types
    Added variables for animal details and displayed their types.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+let animalName;
+let height;
+let isIndoorPet;
 
+document.getElementById("string").innerHTML = typeOf animalName;
+document.getElementById("number").innerHTML = typeOf height;
+document.getElementById("boolean").innerHTML = typeOf isIndoorPet;
