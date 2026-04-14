3483f11ed22877787dfb6e0e4a7696968fa77fb9
Cayleb West <cwest@anamosa.k12.ia.us>
2026-04-14 11:39:10 -0500

    Initialize animal properties and update DOM elements

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let animalName;
-let height;
-let isIndoorPet;
+let animalName = 'dog';
+let height = 8000;
+let isIndoorPet = true;
 
 document.getElementById("string").innerHTML = typeOf animalName;
 document.getElementById("number").innerHTML = typeOf height;
