ae5bef634ad7fe1d6337b8309aac789f97179674
Cayleb West <cwest@anamosa.k12.ia.us>
2026-04-14 11:33:51 -0500

    Fix variable declaration and update animalName assignment

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ let isIndoorPet;
 document.getElementById("string").innerHTML = typeOf animalName;
 document.getElementById("number").innerHTML = typeOf height;
 document.getElementById("boolean").innerHTML = typeOf isIndoorPet;
-document.getElementById('result').innerHTML = animalName;
+const animalName = document.getElementById('result').innerHTML;
