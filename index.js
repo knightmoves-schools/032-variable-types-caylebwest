58e2055aae93c216fae1827deb5a355054d90d67
Cayleb West <cwest@anamosa.k12.ia.us>
2026-04-14 11:40:37 -0500

    Fix variable usage and update HTML content

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-let animalName = 'dog';
+let animalName = "Dog";
 let height = 8000;
 let isIndoorPet = true;
 
-document.getElementById("string").innerHTML = typeOf animalName;
-document.getElementById("number").innerHTML = typeOf height;
-document.getElementById("boolean").innerHTML = typeOf isIndoorPet;
 document.getElementById("result").innerHTML = animalName;
